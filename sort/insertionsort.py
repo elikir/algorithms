4443750760b397adea90a55be7b5187db7c6617f
elikir <ekirmayer18@gmail.com>
2016-11-09 12:42:11 -0500

    finished up insertion sort with some logic help from the internet.  NOTE: do not swap everything until you reach, merely find where you should swap and swap 2 positions.  Also added 2 runs.  Quite efficient, will work on selection sort next.

--- sort/insertionsort.py
+++ sort/insertionsort.py
@@ -1,18 +1,34 @@
 import time
 import random
+
 def sort(lis):
+    for x in range(1,len(lis)):
+        curVal = lis[x]
+        pos = x
+
+        while 0 < pos and lis[pos-1] > curVal:
+            lis[pos] = lis[pos-1]
+            pos -= 1
+        lis[pos] = curVal
+    return lis
 
 
 
+
+
+#run #1 on list of length 100 - 0.000652874565125 seconds
+
+#run #2 on list of length 100 - 0.00066021232605 seconds
 
 if __name__ == "__main__":
     outside_average = 0
     for x in range(100):
         inside_average = 0
         for y in range(100):
-            lis = range(1000000)
+            lis = range(100)
+            random.shuffle(lis)
             start = time.time()
-            sort(random.shuffle(lis))
+            sort(lis)
             end = time.time()
             inside_average += end-start
         outside_average += inside_average/100
